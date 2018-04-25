50e5df263ff4020fd96b9069ffa4065434b0334a
Mohamed Selim <mohamedstoom@gmail.com>
2018-04-25 05:47:40 +0200

    final files commit

---
+++ Super Unhider/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Multiselect = True
+        Me.OpenFileDialog1.Title = "Choose Files"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(57, 269)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(162, 56)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Add Files"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(26, 345)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(101, 35)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Unhide"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 12)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(253, 238)
+        Me.ListBox1.TabIndex = 2
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(150, 345)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(101, 35)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Rename"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(277, 402)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Super Unhider"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Public WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ Super Unhider/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        For i = 1 To ListBox1.Items.Count
+            Process.Start("cmd", "/c attrib -s -h """ & ListBox1.SelectedItem.ToString & """")
+            If ListBox1.SelectedIndex = ListBox1.Items.Count - 1 Then
+                Exit For
+            Else
+                ListBox1.SelectedIndex = ListBox1.SelectedIndex + +1
+            End If
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If OpenFileDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            For Each sFile As String In OpenFileDialog1.FileNames
+                Dim pos As Integer = sFile.LastIndexOf("\"c)
+                Dim File As String = Replace(sFile.ToString, System.IO.Path.GetFileName(sFile), "g" & System.IO.Path.GetFileName(sFile))
+                Me.ListBox1.Items.Add(File)
+                ListBox1.SelectedIndex = ListBox1.SelectedIndex + +1
+            Next
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        For Each sFile As String In OpenFileDialog1.FileNames
+            Process.Start("cmd", "/c rename """ & Replace(sFile.ToString, System.IO.Path.GetFileName(sFile), "g" & System.IO.Path.GetFileName(sFile)) & """ " & System.IO.Path.GetFileName(sFile))
+            If ListBox1.SelectedIndex = ListBox1.Items.Count - 1 Then
+                Exit For
+            Else
+                ListBox1.SelectedIndex = ListBox1.SelectedIndex + +1
+            End If
+        Next
+    End Sub
+End Class
